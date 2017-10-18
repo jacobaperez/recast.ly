6ac36cada83a35313f5eab93e06365d34737bf0e
jacobaperez <jacobanthony.perez@gmail.com>
2017-10-18 15:41:36 -0700

    onClick for each videoEntry done, videoList rendering on App

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,19 +1,21 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videos = props.videos;
+    // console.log(this.videos, props.videos);
+  }
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
-VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
-};
+  render() {
+    return (
+      <div className='video-list'>
+        {/*for each video map a videolist entry*/}
+        { this.videos.map(video => {
+           console.log(video);
+           return <VideoListEntry title={video.snippet.title} desc={video.snippet.description} thumbnail={video.snippet.thumbnails.default.url} />
+          })
+        }
+      </div>
+    );
+  }
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope.
-// `var` declarations will only exist globally where explicitly defined.
-window.VideoList = VideoList;
+}
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,21 +1,59 @@
-var VideoListEntry = () => (
+class VideoListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.title = props.title;
+    this.thumbnail = props.thumbnail;
+    this.description = props.desc;
+
+    this.state = {
+      done:false
+    }
+  }
+
+  onVideoClick(event) {
+    this.setState({
+      done: !this.state.done
+    });
+    console.log('I got clicked', event);
+  }
+
+  render() {
+    return (
+      <div className="video-list-entry media">
+        <div className="media-left media-middle">
+          <img onClick={this.onVideoClick.bind(this)} className="media-object" src={this.thumbnail} alt="" />
+        </div>
+        <div className="media-body">
+          <div onClick={this.onVideoClick.bind(this)} className="video-list-entry-title">{this.title}</div>
+          <div className="video-list-entry-detail">{this.description}</div>
+        </div>
+      </div>   
+    );
+  }
+}
+
+
+
+
+/*var VideoListEntry = (props) => (
+  
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={props.thumbnail} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{props.title}</div>
+      <div className="video-list-entry-detail">{props.desc}</div>
     </div>
   </div>
-);
+);*/
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
-VideoListEntry.propTypes = {
-  video: React.PropTypes.object.isRequired
-};
+// // PropTypes tell other developers what `props` a component expects
+// // Warnings will be shown in the console when the defined rules are violated
+// VideoListEntry.propTypes = {
+//   video: React.PropTypes.object.isRequired
+// };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
-window.VideoListEntry = VideoListEntry;
+// // In the ES6 spec, files are "modules" and do not share a top-level scope
+// // `var` declarations will only exist globally where explicitly defined
+// window.VideoListEntry = VideoListEntry;
